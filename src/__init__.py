fabebacf6178a215ee1467e0877cd058fe54cc36
Marco Jorge H. Sardido <marcosardido@entropys-MacBook-Pro.local>
2023-04-13 16:35:41 +0800

    Added 'certifi' package to fix mongodb connection issue

--- src/__init__.py
+++ src/__init__.py
@@ -1,19 +1,34 @@
 from flask import Flask
-import pymongo 
+import certifi
 
-from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv())
+from pymongo import MongoClient
+
 import os
+from dotenv import load_dotenv, find_dotenv
 
 # init app
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+load_dotenv(find_dotenv())
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 # setup MongoDB
-connection_string = "mongodb+srv://msardidodev:n2EYar36WsPydAnA@todoapp.xcboila.mongodb.net/TodoApp?retryWrites=true&w=majority"
+connection = os.environ.get('MONGO_URI')
+# connection = os.environ.get('MONGO_CONTAINER_URI')
 
-client = pymongo.MongoClient(connection_string, serverSelectionTimeoutMS=5000)
-db = client.db
+# Create a new client and connect to the server
+client = MongoClient(connection, tlsCAFile=certifi.where())
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+myDB = client['todo_data']
+todoCollection = myDB['todos']
+
 
 from src import routes
