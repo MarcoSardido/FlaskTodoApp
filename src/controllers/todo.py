16a37b9275b5b53e1bf0261f63512858f24e4210
Marco Jorge H. Sardido <marcosardido@entropys-MacBook-Pro.local>
2023-04-18 13:33:22 +0800

    Modified routes to be restful

--- src/controllers/todo.py
+++ src/controllers/todo.py
@@ -9,34 +9,28 @@ from src.forms import TodoForm
 # Models
 from src.models.todo import Todo
 
-
-# ========== Main Page ==========
 @app.route('/')
 def get_todo():
     todo_array = []
-    todos = Todo().getList()
+    todos = Todo.getList()
     for todo in todos:
         todo['date_created'] = todo["created"].strftime("%d/%m/%Y, %H:%M:%S")
         todo_array.append(todo)
     return render_template('todo.html', todo_array=todo_array)
 
-
-# ========== Add Page ==========
-# Page Route
-@app.route('/add_todo', methods=['GET'])
+@app.route('/todo', methods=['GET'])
 def view_add_todo():
     form = TodoForm()
     return render_template('add_todo.html', form=form)
 
-# Create
-@app.route('/add_todo', methods=['POST'])
+@app.route('/todo', methods=['POST'])
 def add_todo():
     form = TodoForm(request.form)
     todo_name = form.name.data
     todo_description = form.description.data
     todo_completed = form.completed.data
     try:
-        Todo().create({
+        Todo.create({
         "title": todo_name,
         "description": todo_description,
         "completed": todo_completed
@@ -49,12 +43,9 @@ def add_todo():
         logging.error('error creating todo')
         return jsonify({'error': ex.message})
     
-
-# ========== Update Todo ==========
-# Route
-@app.route('/update_todo/<id>', methods=['GET'])
+@app.route('/todo/<id>', methods=['GET'])
 def view_update_todo(id):
-    todo = Todo().get(id)
+    todo = Todo.get(id)
     if todo is None:
         logging.error('Todo does not exist')
         return jsonify({'error': 'Todo does not exist'})
@@ -70,17 +61,15 @@ def view_update_todo(id):
         logging.error('error updating todo')
         return jsonify({'error': ex.message})
 
-    
-# Update
-@app.route('/update_todo/<id>', methods=['POST'])
+@app.route('/todo/<id>', methods=['POST'])
 def update_todo(id):
-    todo = Todo()
+    todo = Todo.get(id)
     form = TodoForm(request.form)
     todo_name = form.name.data
     todo_description = form.description.data
     todo_completed = form.completed.data
     try:
-        todo.get(id).update({
+        todo.update({
             "title": todo_name,
             "description": todo_description,
             "completed": todo_completed
@@ -93,17 +82,14 @@ def update_todo(id):
         logging.error('error creating todo')
         return jsonify({'error': ex.message})
  
-
-# ========== Delete Todo ==========
-@app.route('/delete_todo/<id>')
+@app.route('/todo/<id>/delete',  methods=['POST'])
 def delete_todo(id):
-    todo = Todo()
-    todo.get(id)
+    todo = Todo.get(id)
     if todo is None:
         logging.error('Todo does not exist')
         return jsonify({'error': 'Todo does not exist'})
     try:
-        todo.get(id).delete()
+        todo.delete()
         flash('Todo Successfully Deleted!', 'success')
         return redirect('/')
     except Exception as ex:
